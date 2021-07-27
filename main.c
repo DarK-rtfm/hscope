500af8f313232e0b93f588f71919cc4cf85645ab
DarK-rtfm <44683230+DarK-rtfm@users.noreply.github.com>
2021-07-27 20:58:15 +0200

    makefile++ and symlink that assumes notification

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 #include <libnotify/notify.h>
 int main(int argc, char** argv) {
     char* type = NULL;
-    int notif = 0;
+    int notif = !strcmp(*argv, "hscopen");
     for (int i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "-n")) {
             notif=1;}
         else type=argv[i];
     }
-    if (type == NULL) {fprintf(stderr, "Usage: %s [-n] csillagjegy\n", *argv); return 2;}
+    if (type == NULL) {fprintf(stderr, "Usage: %s %scsillagjegy\n", *argv, strcmp(*argv, "hscopen") ? "[-n] " : ""); return 2;}
     char* scope = getscope(type);
 
     if (notif) {
         NotifyNotification *n;
-        notify_init("CSCOPE");
+        notify_init("HSCOPE");
         n = notify_notification_new (scope, NULL, NULL);
         if (!notify_notification_show (n, NULL)) {
 			fprintf(stderr, "Failed to send notification.");
